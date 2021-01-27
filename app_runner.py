2bb649201901a09764dde2e71b582fa5f35823be
cschaefer26 <c.schaefer.home@gmail.com>
2021-01-27 13:20:11 +0100

    Removed unnecessary region.

--- app_runner.py
+++ app_runner.py
@@ -1,17 +1,11 @@
-import time
-import os
-import boto3
-from flask import Flask, request, jsonify, render_template
-import json
-import os
-import time
-import boto3
-from botocore.client import BaseClient
-import logging
 import logging.config
-import yaml
-from classifier.model import TextClassifier
+import os
 
+import boto3
+import yaml
+from flask import Flask, request, render_template
+
+from classifier.model import TextClassifier
 
 app = Flask(__name__)
 
@@ -26,31 +20,16 @@ def my_form_post():
     text = request.form['text']
     app.logger.info(f'Processing classification request with text: {text}')
     result = classifier(text)
-    #result = jsonify(result)
-    #classes = sorted(list(result.keys()), key=result.get, reverse=True)
-    #result_string = ''.join([f'{c}: {result[c]}\n' for c in classes])
     return render_template('form_template.html', classifier_result=result)
 
-
-"""
-@app.route('/classify', methods=['GET'])
-def classify():
-    string = request.args.get('text')
-    app.logger.info(f'Processing classification request with string: {string}')
-    result = classifier(string)
-    result = jsonify(result)
-    return result
-"""
 
 @app.route('/', methods=['GET'])
 def health():
     return 'healthy.'
 
 
-
 if __name__ == '__main__':
     model_bucket_name = os.environ['MODEL_BUCKET_NAME']
-    region = os.environ['AWS_REGION']
     s3_client = boto3.client('s3')
 
     with open('logging-config.yaml', 'r') as f:
