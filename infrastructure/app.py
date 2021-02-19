1642ea3a3ca137fdfeb9df2435d2d26e01a5e879
cschaefer26 <c.schaefer.home@gmail.com>
2021-02-19 16:36:19 +0100

    Removed account id.

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -1,24 +1,16 @@
-from aws_cdk import (
-    core
-)
+from aws_cdk import core
 
 from stacks.cicd_stack import CiCdStack
 from stacks.networking_stack import NetworkingStack
 from stacks.serving_stack import ServingStack
 
-shared_context = {'model_bucket_name': 'classifier-serving-model-bucket',
-                  'aws_region': 'eu-central-1',
-                  'aws_account': 'change-me',
-                  'port': 80,
-                  'github_owner': 'change-me',
-                  'github_repo': 'ClassifierAWS',
-                  'github_connection_arn': 'change-me'}
+app = core.App()
+
+shared_context = app.node.try_get_context('shared_context')
 
 cdk_environment = core.Environment(
     region=shared_context['aws_region'],
     account=shared_context['aws_account'])
-
-app = core.App()
 
 cicd = CiCdStack(scope=app,
                  id='classifier-cicd-stack',
@@ -33,6 +25,5 @@ serving = ServingStack(app,
                        repository=cicd.ecr_repository,
                        env=cdk_environment,
                        shared_context=shared_context)
-
 
 app.synth()
